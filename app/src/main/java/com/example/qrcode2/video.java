97a922ef7687cdc1dbb1537a2e91f7b707209f1f
Barbara <barbaracmatos29@ua.pt>
2021-03-28 23:11:16 +0100

    Videochamada

--- app/src/main/java/com/example/qrcode2/video.java
+++ app/src/main/java/com/example/qrcode2/video.java
@@ -1,8 +1,19 @@
 package com.example.qrcode2;
+//package com.example.jitsimeet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.qrcode2.R;
+
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class video extends AppCompatActivity {
 
@@ -10,7 +21,29 @@ public class video extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        try {
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                    .setServerURL(new URL(""))
+                    //.setRoom("test123")
+                    //.setAudioMuted(false)
+                    //.setVideoMuted(false)
+                    //.setAudioOnly(false)
+                    .setWelcomePageEnabled(false)
+                    .build();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
 
-        getSupportActionBar().hide();
+    public void onButtonClick(View view) {
+        //EditText editText = findViewById(R.id.conferenceName);
+        //String text = editText.getText().toString();
+        String text = "HufChamadas";
+
+        if(text.length() > 0) {
+            JitsiMeetConferenceOptions options
+                    = new JitsiMeetConferenceOptions.Builder().setRoom(text).build();
+            JitsiMeetActivity.launch(this, options);
+        }
     }
 }
